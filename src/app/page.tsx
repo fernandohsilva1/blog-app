6ba9269b1e56433465676c8cae645fcbadf9afb6
fernandohsilva1 <fernandohsilva1@hotmail.com>
2025-10-16 03:08:15 -0300

    create post repository model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import clsx from 'clsx';
+
 export default function HomePage() {
-  return <h2>Página de Next.js</h2>
+  return (
+    <div>
+      <h2 
+        className={clsx(
+          'text-2xl', 
+          'font-bold', 
+          'text-blue-200', 
+          'hover:text-purple-900', 
+          'hover:bg-blue-900', 
+          'transition', 
+          'duration-200',
+        )}
+      >
+          Página de Next.js
+      </h2>
+    </div>
+  )
 }
